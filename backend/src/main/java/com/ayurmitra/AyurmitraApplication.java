08ba5e5a55682f3186db3fa45976f706d820e03d
CodewidSak <sakshi2408jain@gmail.com>
2026-05-28 12:03:51 +0530

    Add Spring Boot main application class

---
+++ backend/src/main/java/com/ayurmitra/AyurmitraApplication.java
@@ -0,0 +1,12 @@
+package com.ayurmitra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AyurmitraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AyurmitraApplication.class, args);
+    }
+}
